b5aec96b1650829635b1a8be462350edfc1e6f7e
Sanjay Patil GN <sanjaypailgn599@gmail.com>
2024-10-19 00:14:59 +0530

    changes made again in mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,31 +2,38 @@ import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
+// Define the type for the cached variable
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Use `unknown` instead of `any` for better type safety
+let cached: MongooseConnection = (global as { mongoose?: MongooseConnection }).mongoose || { 
+  conn: null, 
+  promise: null 
+};
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+if (!cached) {
+  cached = (global as { mongoose?: MongooseConnection }).mongoose = { 
+    conn: null, 
+    promise: null 
+  };
 }
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+      dbName: 'imaginify', 
+      bufferCommands: false 
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
